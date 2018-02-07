1495b6d5588a76a1c0ce186a1eb07587a1f14b5e
Agraynel <Yi Chen @ Cornell>
2018-02-06 22:27:20 -0800

    98/100 submission
    Test 9: create two nested iterators over the same randomized queue
      * n = 10
        - outer iterator failed to return one (or more) of the 10 items
        - outer iterator returned the same item more than once
    
      * n = 50
        - outer iterator failed to return one (or more) of the 50 items
        - outer iterator returned the same item more than once
    
    ==> FAILED
    
    Test 10: create two parallel iterators over the same randomized queue
      * n = 10
        - two iterators return a different set of values
        - they should return the same set of values
          (typically in a different order)
    
      * n = 50
        - two iterators return a different set of values
        - they should return the same set of values
          (typically in a different order)
    
    ==> FAILED
    
    Test 3 (bonus): check that maximum size of any or Deque or
    RandomizedQueue object
                    created is equal to k
      * filename = tale.txt, n = 138653, k = 5
      * filename = tale.txt, n = 138653, k = 50
      * filename = tale.txt, n = 138653, k = 500
      * filename = tale.txt, n = 138653, k = 5000
      * filename = tale.txt, n = 138653, k = 50000
    ==> passed

--- queues/src/RandomizedQueue.java
+++ queues/src/RandomizedQueue.java
@@ -1,2 +1,106 @@
-public class RandomizedQueue {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.lang.UnsupportedOperationException;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] array;
+    private int size;
+
+    public RandomizedQueue() {
+        array = (Item[])new Object[2];
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void resize(int n) {
+        Item[] newArray = (Item[])new Object[n];
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            Item item = array[i];
+            if (item != null) {
+                newArray[j++] = item;
+            }
+        }
+        array = newArray;
+    }
+
+    public void enqueue(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException("Null item!");
+        if (size >= array.length) resize(2 * array.length);
+        array[size++] = item;
+    }
+
+    public Item dequeue() {
+        if (isEmpty())
+            throw new NoSuchElementException("Queue is empty!");
+        Item item = null;
+        int rand = 0;
+        while (item == null) {
+            rand = StdRandom.uniform(size);
+            item = array[rand];
+        }
+        array[rand] = array[size - 1];
+        array[size - 1] = null;
+        size--;
+        if (size > 0 && size <= array.length / 4)
+            resize(array.length / 2);
+        return item;
+    }
+
+    public Item sample() {
+        if (isEmpty())
+            throw new NoSuchElementException("Queue is empty!");
+        int rand = 0;
+        Item item = null;
+        while (item == null) {
+            rand = StdRandom.uniform(size);
+            item = array[rand];
+        }
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new NodeIterator();
+    }
+    // return an iterator over items in order from front to end
+
+    private class NodeIterator implements Iterator<Item> {
+        int curr = size;
+
+        public boolean hasNext() {
+            return curr != 0;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported!");
+        }
+
+        public Item next() {
+            if (!hasNext())
+                throw new NoSuchElementException("No next item!");
+            int rand = 0;
+            Item item = null;
+            while (item == null) {
+                rand = StdRandom.uniform(curr);
+                item = array[rand];
+            }
+            array[rand] = array[--curr];
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }  // unit testing (optional)
 }
