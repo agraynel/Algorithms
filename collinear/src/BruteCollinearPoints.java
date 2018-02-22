1e6ebac02e5e6263d3c862c4e9a80cf8cf8ac7fb
Agraynel <Yi Chen @ Cornell>
2018-02-22 02:24:18 -0800

    Hw 3

--- collinear/src/BruteCollinearPoints.java
+++ collinear/src/BruteCollinearPoints.java
@@ -1,12 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BruteCollinearPoints {
+
+    private List<LineSegment> lineSegments = new ArrayList<>();
+
     public BruteCollinearPoints(Point[] points) { // finds all line segments containing 4 points
+        if (points == null)
+            throw new IllegalArgumentException("null!");
+        for (Point p: points) {
+            if (p == null)
+                throw new IllegalArgumentException("null!");
+        }
+
+        Point[] newPoints = Arrays.copyOf(points, points.length);
+        Arrays.sort(newPoints);
+
+        for (int i = 1; i < newPoints.length; i++) {
+            if (newPoints[i - 1].compareTo(newPoints[i]) == 0)
+                throw new IllegalArgumentException("duplicate!");
+        }
+
+        helper(newPoints);
 
     }
     public int numberOfSegments() { // the number of line segments
-
+        return lineSegments.size();
     }
 
     public LineSegment[] segments() { // the line segments
+        LineSegment[] res = new LineSegment[lineSegments.size()];
+        for (int i = 0; i < lineSegments.size(); i++) {
+            res[i] = lineSegments.get(i);
+        }
+        return res;
+    }
 
+    private void helper(Point[] points) {
+        for (int i = 0; i < points.length; i++) {
+            Point p1 = points[i];
+            for (int j = i + 1; j < points.length; j++) {
+                Point p2 = points[j];
+                for (int k = j + 1; k < points.length; k++) {
+                    Point p3 = points[k];
+
+                    if (p1.slopeTo(p2) == p1.slopeTo(p3)) {
+                        for (int l = k + 1; l < points.length; l++) {
+                            Point p4 = points[l];
+                            if (p1.slopeTo(p2) == p1.slopeTo(p4)) {
+
+                                Point[] sortPoints = {p1, p2, p3, p4};
+                                Arrays.sort(sortPoints);
+                                lineSegments.add(new LineSegment(sortPoints[0], sortPoints[3]));
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- collinear/src/FastCollinearPoints.java
+++ collinear/src/FastCollinearPoints.java
@@ -1,12 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class FastCollinearPoints {
+    private List<LineSegment> lineSegments = new ArrayList<>();
     public FastCollinearPoints(Point[] points) { // finds all line segments containing 4 or more points
+        if (points == null)
+            throw new IllegalArgumentException("null!");
+        for (Point p: points) {
+            if (p == null)
+                throw new IllegalArgumentException("null!");
+        }
 
+        Point[] newPoints = Arrays.copyOf(points, points.length);
+        Arrays.sort(newPoints);
+
+        for (int i = 1; i < newPoints.length; i++) {
+            if (newPoints[i - 1].compareTo(newPoints[i]) == 0)
+                throw new IllegalArgumentException("duplicate!");
+        }
+
+        helper(newPoints);
     }
-    public int numberOfSegments() { // the number of line segments{
 
+    public int numberOfSegments() { // the number of line segments{
+        return lineSegments.size();
     }
 
     public LineSegment[] segments() { // the line segments
+        LineSegment[] res = new LineSegment[lineSegments.size()];
+        for (int i = 0; i < lineSegments.size(); i++) {
+            res[i] = lineSegments.get(i);
+        }
+        return res;
+    }
 
+    private void helper(Point[] points) {
+        for (int i = 0; i < points.length; i++) {
+            Point p = points[i];
+            Comparator<Point> slopeOrder = p.slopeOrder();
+            Point[] newPoints = new Point[points.length];
+            System.arraycopy( points, 0, newPoints, 0, points.length );
+            Arrays.sort(newPoints, slopeOrder);
+
+            int start = 1, end = 2;
+            while (end < newPoints.length) {
+                if (slopeOrder.compare(newPoints[start], newPoints[end]) == 0) {
+                    end++;
+
+                } else {
+                    if (end - start >= 3) {
+                        if (newPoints[start].compareTo(newPoints[start + 1]) < 0)
+                            lineSegments.add(new LineSegment(newPoints[start], newPoints[end - 1]));
+                    }
+                    start = end;
+                    end++;
+                }
+            }
+        }
     }
 }
